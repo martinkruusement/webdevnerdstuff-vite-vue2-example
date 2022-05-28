eaeca831fffec8b8569e2a5b50f976004f7cfe11
Martin Kruusement <martinkruusement@gmail.com>
2022-05-28 04:30:41 +0300

    fixed it

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+	<div>
+		<h1>{{ msg }}</h1>
+
+		<p>
+			Recommended IDE setup:
+			<a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
+			+
+			<a
+				href="https://marketplace.visualstudio.com/items?itemName=octref.vetur"
+				target="_blank"
+				>Vetur</a
+			>
+		</p>
+
+		<p>
+			<a href="https://vitejs.dev/guide/features.html" target="_blank">
+				Vite Documentation
+			</a>
+			|
+			<a href="https://v2.vuejs.org/" target="_blank">Vue 2 Documentation</a>
+		</p>
+
+		<button type="button" @click="count++">count is: {{ count }}</button>
+		<p>
+			Edit
+			<code>components/HelloWorld.vue</code> to test hot module replacement.
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'HelloWorld',
+	props: {
+		msg: String,
+	},
+	data() {
+		return {
+			count: 0,
+		};
+	},
+};
+</script>
+
+<style scoped>
+a {
+	color: #42b983;
+}
+</style>
